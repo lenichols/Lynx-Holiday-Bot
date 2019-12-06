647db58b9c5c947079bc1af2fb8d20861b018635
L.E. Nichols <le_nichols@yahoo.com>
2019-12-05 17:16:53 -0800

    gitignore fix

---
+++ build/precache-manifest.281eba449864f466834d58ae2fd3fc6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a42db0c0c22b3912ff8ed45ced07ab",
+    "url": "/Lynx-Holiday-Bot/index.html"
+  },
+  {
+    "revision": "6e6b9253e55398e1442f",
+    "url": "/Lynx-Holiday-Bot/static/css/2.bfc8b4c3.chunk.css"
+  },
+  {
+    "revision": "13d0caa2f5b563e1256d",
+    "url": "/Lynx-Holiday-Bot/static/css/main.cb86f0cb.chunk.css"
+  },
+  {
+    "revision": "6e6b9253e55398e1442f",
+    "url": "/Lynx-Holiday-Bot/static/js/2.74ed2330.chunk.js"
+  },
+  {
+    "revision": "13d0caa2f5b563e1256d",
+    "url": "/Lynx-Holiday-Bot/static/js/main.4b418c7f.chunk.js"
+  },
+  {
+    "revision": "147c24d670c72d7ddd15",
+    "url": "/Lynx-Holiday-Bot/static/js/runtime-main.a3e6df63.js"
+  },
+  {
+    "revision": "d23c8b0eccb53b2568546915db7e33f4",
+    "url": "/Lynx-Holiday-Bot/static/media/LynxLogo.d23c8b0e.svg"
+  },
+  {
+    "revision": "913ec13aa64d28dd7d7568e42e961c93",
+    "url": "/Lynx-Holiday-Bot/static/media/holdiday-bkg.913ec13a.jpg"
+  }
+]);
